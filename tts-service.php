fb1c147b014598a303ad1423df09951f6c5e7c52
Esche <eschenbach@gmail.com>
2015-08-28 14:13:35 +0200

    PHP Script to execute Python Script
    With this script you can execute the python script over a webservice

---
+++ tts-service.php
@@ -0,0 +1,50 @@
+<?php
+ header('Content-type: text/plain; charset=utf-8');
+ #error_reporting(E_ALL);
+ #ini_set('display_errors', 1);
+	
+	
+ # CONFIGURATION
+ define("COMMAND_USER", 'sudo -u tts');
+ define("COMMAND_FILE", '/opt/tts/pi-to-speech-service.py');
+ define("COMMAND_EXEC", COMMAND_USER . ' ' . COMMAND_FILE);
+	
+ init();
+ 
+ function init(){	
+ 	 $cmdParams = initParams();
+
+ 	 executeTTS($cmdParams);
+ }
+ 
+ function initParams(){
+		$paramQuery = $_SERVER['QUERY_STRING'];  
+		parse_str($paramQuery, $queryParams); #parses the query string into an array
+		
+		if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    			$queryParams['text'] = @file_get_contents('php://input');
+		}else{
+				if(!isset($queryParams['text'])) {
+					$queryParams['text'] =  urldecode($paramQuery);
+  			}
+		}
+	
+	  $cmdParams = "";
+		foreach($queryParams as $key => $value) {			
+			$cmdParams .= (strlen($key)>2?' --' : ' -').$key;
+			if($value != ''){
+				$cmdParams .= '="'.$value.'"';
+			}
+		}
+
+ 		return $cmdParams;
+ }
+
+ function executeTTS($cmdParams){	  
+		$command = escapeshellcmd(COMMAND_EXEC . $cmdParams);
+		#echo "execute command: " . $command;
+		
+		$output = shell_exec($command);
+		echo $output;	
+ }
+?>
